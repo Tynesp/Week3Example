2528debe7504d5ef67524c01e26b11297bc35906
nicollti <nicollti@sheridancollege.ca>
2023-05-25 14:31:18 -0400

    version 1

---
+++ src/week3example/Week3Example.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week3example;
+
+/**
+ *
+ * @author tenes
+ */
+public class Week3Example {
+//push steps 1. intitalize versioning, .gitconfig file, .gitignore, commit local respository
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic heresout
+        System.out.println("Helloworld");
+    }
+    
+}
